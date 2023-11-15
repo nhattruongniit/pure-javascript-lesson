b8c2be014ff67a5bdb5b4a2ca0cdc582e899a8dc
truong.nn <truong.nn@amanotes.com>
2023-11-15 10:21:27 +0700

    add form user

--- form-user/assets/js/index.js
+++ form-user/assets/js/index.js
@@ -1,4 +1,5 @@
 const submitForm = document.getElementById("submitForm");
+const tableUser = document.getElementById("tableUser");
 
 // get value of input
 function getValue(id) {
@@ -22,4 +23,33 @@ submitForm.addEventListener("submit", function (e) {
   ) {
     showError("firstName", "Vui lòng kiểm tra lại first name");
   }
+
+  renderUser([1]); // demo
 });
+
+function renderUser(dataSource) {
+  tableUser.innerHTML = "";
+
+  dataSource.forEach((user, index) => {
+    tableUser.innerHTML += `
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">Email</th>
+        <th scope="col">Gender</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>tony@gmail.com</td>
+        <td>Male</td>
+      </tr>
+    </tbody>
+  `;
+  });
+}
