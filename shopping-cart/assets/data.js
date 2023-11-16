399b4bf5fbebfb4ee9cde16d8cbde13a44d2d527
truong.nn <truong.nn@amanotes.com>
2023-11-16 09:10:21 +0700

    shopping cart

---
+++ shopping-cart/assets/data.js
@@ -0,0 +1,26 @@
+const mockShoes = [
+  {
+    id: Math.ceil(Math.random() * 1000),
+    color: "rgb(212, 215, 214)",
+    image:
+      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png",
+    name: "Nike Air Zoom Pegasus 36",
+    description: ` The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh
+    that targets breathability across high-heat areas. A slimmer heel
+    collar and tongue reduce bulk, while exposed cables give you a
+    snug fit at higher speeds.`,
+    price: 120,
+  },
+  {
+    id: Math.ceil(Math.random() * 1000),
+    color: "rgb(34, 175, 220)",
+    image:
+      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1315882/air-zoom-pegasus-36-mens-running-shoe-wide-D24Mcz-removebg-preview.png",
+    name: "Nike Air Zoom Pegasus 36",
+    description: ` The iconic Nike Air Zoom Pegasus 36 offers more cooling and mesh
+    that targets breathability across high-heat areas. A slimmer heel
+    collar and tongue reduce bulk, while exposed cables give you a
+    snug fit at higher speeds.`,
+    price: 108.97,
+  },
+];
